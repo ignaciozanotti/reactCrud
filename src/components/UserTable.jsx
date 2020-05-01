3eee9649176f23a5f1d19ae2ec58fc599d64cbd1
ignaciozanotti <ignacio.zanotti@gmail.com>
2020-05-01 16:33:30 -0300

    dynamic users display (no add)

--- src/components/UserTable.jsx
+++ src/components/UserTable.jsx
@@ -15,17 +15,22 @@ const UserTable = (props) => {
     </thead>
     <tbody>
 {
+  props.users.length > 0 ?
   props.users.map( user => (
-    <tr>
-    <td>Name data</td>
-    <td>Username data</td>
+    <tr key={user.id}>
+    <td>{user.name}</td>
+    <td>{user.username}</td>
     <td>
       <button className="button muted-button">Edit</button>
       <button className="button muted-button">Delete</button>
     </td>
   </tr>
 
-  ))
+  )): (
+    <tr>
+      <td>No users</td>
+    </tr>
+  )
 }
 
 
